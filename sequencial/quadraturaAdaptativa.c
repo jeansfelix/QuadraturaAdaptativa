f9d43e6892ab0904cb64614e406e5b185132dc23
jfelix <jean.s.felix@gmail.com>
2015-11-29 00:17:24 -0200

    Tornando o código sequencial iterativo e não mais recursivo

--- sequencial/quadraturaAdaptativa.c
+++ sequencial/quadraturaAdaptativa.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 long double umMaisX(long double x)
 {
@@ -10,48 +11,93 @@ long double umMaisX(long double x)
     return retorno;
 }
 
+long double raizDeUmMaisXElevadoAoQuadrado(long double x)
+{
+    long double retorno;
+
+    retorno = sqrt(1.0L + x*x);
+    
+    return retorno;
+}
+
+long double raizDeUmMaisXElevadoAQuartaPotencia(long double x)
+{
+    long double retorno;
+
+    retorno = sqrt(1.0L + x*x*x*x);
+    
+    return retorno;
+}
+
 long double calculaIntervalo(long double inicio, long double fim, long double erro, long double (*func)(long double) )
 {
     long double pontoMedio_InicioFim, pontoMedio_InicioMeio, pontoMedio_MeioFim;
     long double valorFunc_PontoMedio_InicioFim, valorFunc_PontoMedio_InicioMeio, valorFunc_PontoMedio_MeioFim;
     long double intervalo_InicioFim, intervalo_InicioMeio, intervalo_MeioFim;
     long double areaAprox_InicioFim, areaAprox_InicioMeio, areaAprox_MeioFim;
-    long double areaAprox;
+
+    long double erroAtual;
+    long double inicioAtual, fimAtual;
+    long double somaPartesInicioAteFim;
     
-    pontoMedio_InicioFim = (fim + inicio) / 2.0L;
-    pontoMedio_InicioMeio = (inicio + pontoMedio_InicioFim) / 2.0L;
-    pontoMedio_MeioFim = (pontoMedio_InicioFim + fim) / 2.0L;
+    inicioAtual = inicio;
+    fimAtual = fim;
+    somaPartesInicioAteFim = 0.0L;
+    
+    while(inicioAtual != fim)
+    {   
+        pontoMedio_InicioFim    = (fimAtual + inicioAtual) / 2.0L;
+        pontoMedio_InicioMeio   = (inicioAtual + pontoMedio_InicioFim) / 2.0L;
+        pontoMedio_MeioFim      = (pontoMedio_InicioFim + fimAtual) / 2.0L;
 
-    intervalo_InicioFim     = fim - inicio;
-    intervalo_InicioMeio    = pontoMedio_InicioFim - inicio;
-    intervalo_MeioFim       = fim - pontoMedio_InicioFim;
+        intervalo_InicioFim     = fimAtual - inicioAtual;
+        intervalo_InicioMeio    = pontoMedio_InicioFim - inicioAtual;
+        intervalo_MeioFim       = fimAtual - pontoMedio_InicioFim;
 
-    valorFunc_PontoMedio_InicioFim = (func)(pontoMedio_InicioFim);
-    valorFunc_PontoMedio_InicioMeio = (func)(pontoMedio_InicioMeio);
-    valorFunc_PontoMedio_MeioFim = (func)(pontoMedio_MeioFim);
+        valorFunc_PontoMedio_InicioFim  = (func)(pontoMedio_InicioFim);
+        valorFunc_PontoMedio_InicioMeio = (func)(pontoMedio_InicioMeio);
+        valorFunc_PontoMedio_MeioFim    = (func)(pontoMedio_MeioFim);
 
-    areaAprox_InicioFim     = intervalo_InicioFim * valorFunc_PontoMedio_InicioFim;
-    areaAprox_InicioMeio    = intervalo_InicioMeio * valorFunc_PontoMedio_InicioMeio;
-    areaAprox_MeioFim       = intervalo_MeioFim * valorFunc_PontoMedio_MeioFim;
+        areaAprox_InicioFim     = intervalo_InicioFim * valorFunc_PontoMedio_InicioFim;
+        areaAprox_InicioMeio    = intervalo_InicioMeio * valorFunc_PontoMedio_InicioMeio;
+        areaAprox_MeioFim       = intervalo_MeioFim * valorFunc_PontoMedio_MeioFim;
 
-    if ((areaAprox_InicioFim - (areaAprox_InicioMeio + areaAprox_MeioFim)) > erro) 
-    {
-        areaAprox = calculaIntervalo(inicio, pontoMedio_InicioFim, erro, func) + calculaIntervalo(pontoMedio_InicioFim, fim, erro, func);
+        erroAtual = (areaAprox_InicioFim - (areaAprox_InicioMeio + areaAprox_MeioFim));
         
-        return areaAprox;
+        if (erroAtual < 0.0L) erroAtual = -erroAtual;
+        
+        if (erroAtual <= erro) 
+        {
+            //Soma a area da parte atual e avança para a próxima parte não calculada.
+            somaPartesInicioAteFim  += areaAprox_InicioFim;
+            
+            inicioAtual = fimAtual;
+            fimAtual = inicio + (2.0L * (inicioAtual - inicio));
+            
+            if ( fimAtual >= fim ) 
+            {
+                fimAtual = fim;
+            }
+        }
+        else 
+        {
+            fimAtual = pontoMedio_InicioFim;
+        }
     }
     
-    return areaAprox_InicioFim;
+    return somaPartesInicioAteFim;
 }
 
 int main (int argc, char *argv[])
 {
     long double integral_A_B;
-    long double inicio, fim, erro; 
+    long double inicio, fim, erro;
     
-    if (argc != 4) 
+    int funcao = 0;
+    
+    if (argc < 5) 
     {
-        printf("Número de argumentos incompatível tente: prog valorInicioIntervalo valorFimIntervalo valorErro.\nExemplo: para um intervalo de 0 a 1 e um erro de 0.0001 execute: prog 1 2 0.0001\n ");
+        puts("Número de argumentos incompatível tente: prog valorInicioIntervalo valorFimIntervalo valorErro codFunc.\nExemplo: para um intervalo de 0 a 1, um erro de 0.0001 e a funcao f = 1 + x:\n-> execute: prog 1 2 0.0001 1");
         return -1;
     }
     
@@ -59,9 +105,25 @@ int main (int argc, char *argv[])
     fim = strtold(argv[2], NULL);
     erro = strtold(argv[3], NULL);
     
-    integral_A_B = calculaIntervalo(inicio, fim, erro, &umMaisX);
+    funcao = atoi(argv[4]);
     
-    printf("Valor da integral: %Lf\n", integral_A_B);
+    switch(funcao) 
+    {
+        case 1:
+            integral_A_B = calculaIntervalo(inicio, fim, erro, &umMaisX);
+            break;
+        case 2:
+            integral_A_B = calculaIntervalo(inicio, fim, erro, &raizDeUmMaisXElevadoAoQuadrado);
+            break;
+        case 3:
+            integral_A_B = calculaIntervalo(inicio, fim, erro, &raizDeUmMaisXElevadoAQuartaPotencia);
+            break;
+        default: 
+            puts("Valores válidos para funcao:\n-- f(x) = 1 + x \t\t-> 1\n-- f(x) = sqrt(1 + x^2) \t-> 2\n-- f(x) = sqrt(1 + x^4) \t-> 3");
+            return -1;
+    }
+    
+    printf("Valor da integral: %.20Lf\n", integral_A_B);
     
     return 0;
 }
